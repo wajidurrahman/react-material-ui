98d1cc4ff491826c7419fec5cedb580d800de702
Wajidur Rahman <wrfuad0@gmail.com>
2021-04-14 11:39:55 +0600

    introduction to axios advance data loading system

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import { Button } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 import './App.css';
 import News from './component/News/News';
+import axios from 'axios';
 
 function App() {
   const [articles, setArticles] = useState([]);
-  useEffect(() => {
-      const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=d46fdf0d044444f58a6c061f8399e099'
-      fetch(url)
-      .then(res => res.json())
-      .then(data => setArticles(data.articles))
+  // useEffect(() => {
+  //     const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=d46fdf0d044444f58a6c061f8399e099'
+  //     fetch(url)
+  //     .then(res => res.json())
+  //     .then(data => setArticles(data.articles))
+  // },[])
+  useEffect( () => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=d46fdf0d044444f58a6c061f8399e099'
+    axios(url)
+    .then(data => setArticles(data.data.articles))
   },[])
   return (
     <div>
